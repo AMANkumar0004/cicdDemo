9c048718823483d028df5202fe09f556e6fd0705
Aman Kumar <aman1222010@jmit.ac.in>
2025-08-27 14:58:27 +0530

    Add demo files

---
+++ sum.test.js
@@ -0,0 +1,13 @@
+import sum from "./sum.js";
+
+
+describe("test for a fnx",()=>{
+test('adds 2+2 to equal 4', () => { 
+    expect(sum(2,2)).toBe(4)
+; })
+test('adds -5-5 equal -10', () => { 
+    expect(sum(-5,-5)).toBe(-10)
+; })
+})
+
+
